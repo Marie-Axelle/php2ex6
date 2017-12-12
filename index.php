40ae4bb40cdabb46f229aef08b0bc83f4268f3a0
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-12 14:54:23 +0100

    ex 6

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php
+  $monAge= 30;
+
+  if ($monAge >= 18) {
+    echo 'Tu es majeur';
+  }
+  else {;
+    echo 'Tu n\'es pas majeur';
+  }
+  ?>
+</body>
+</html>
